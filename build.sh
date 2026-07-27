3fb3ed90f64e2ddc695ce447d4d9a46aafa658fc
Auska <luodan0709@live.cn>
2026-07-27 16:57:53 +0800

    CAS: add explicit -lpng -lz link via sed instead of modifying gdlib.pc

--- build.sh
+++ build.sh
@@ -332,19 +332,6 @@ cd "/build/libgd-${GD_VERSION}"
     CFLAGS="${BASE_CFLAGS}" CXXFLAGS="${BASE_CFLAGS}"
 make -j"$(nproc)"
 make install
-# gdlib.pc must expose -lpng -lz in Libs: for static linking of aMule's CAS
-GD_LIB_VERSION="${GD_VERSION#gd-}"
-cat > /usr/local/lib/pkgconfig/gdlib.pc << GD_PC_EOF
-prefix=/usr/local
-exec_prefix=\${prefix}
-libdir=\${exec_prefix}/lib
-includedir=\${prefix}/include
-Name: gdlib
-Description: GD graphics library
-Version: ${GD_LIB_VERSION}
-Libs: -L\${libdir} -lgd -lpng -lz
-Cflags: -I\${includedir}
-GD_PC_EOF
 
 # ---------- 3.14 Crypto++ ----------
 cd "/build/cryptopp-modern-${CRYPTOPP_VERSION}"
@@ -391,6 +378,8 @@ fi
 mkdir -p build && cd build
 # cryptopp-modern uses a different version scheme; bypass the strict version check
 sed -i 's/set (MIN_CRYPTOPP_VERSION 5.6)/set (MIN_CRYPTOPP_VERSION 0.0)/' ../CMakeLists.txt
+# CAS needs explicit -lpng -lz (gdlib.pc doesn't expose transitive deps without --static)
+sed -i 's/target_link_libraries (cas$/target_link_libraries (cas\n\tPRIVATE -lpng -lz/' ../src/utils/cas/CMakeLists.txt
 cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF \
     -DBUILD_WEBSERVER=ON -DBUILD_CAS=ON -DENABLE_NLS=OFF -DBUILD_MONOLITHIC=OFF \
     -DBUILD_REMOTEGUI=OFF -DBUILD_DAEMON=ON -DBUILD_WXCAS=OFF -DBUILD_ALCC=ON \
